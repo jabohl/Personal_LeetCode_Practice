71ed85a7f8075e07aea2b9ddec8d98b9026c9006
James Bohl <94003824+jabohl@users.noreply.github.com>
2026-05-30 00:11:07 -0400

    Update SmallestInfiniteSet.py

--- Arrays/SmallestInfiniteSet.py
+++ Arrays/SmallestInfiniteSet.py
@@ -1,24 +1,40 @@
 class SmallestInfiniteSet:
 
     def __init__(self):
+        
+        
         self.mySet=[]
-        for i in range(1,1001):
-            self.mySet.append(i)
+        
+        for index in range(1,1001):
+            
+            self.mySet.append(index)
         
     def popSmallest(self) -> int:
+        
+        
         val = self.mySet[0]
-        self.mySet=self.mySet[1:]
+        
+        
+        self.mySet = self.mySet[1:]
+        
+        
         return val
 
 
     def addBack(self, num: int) -> None:
-        test=0
-        booleanVal = num not in self.mySet
-        if booleanVal:
+        
+        checker=0
+        
+        checkIn = num not in self.mySet
+        
+        if checkIn:
             
-            for i in range(len(self.mySet)):
-                if self.mySet[i] >  num:
-                    test=i
+            for index in range(len(self.mySet)):
+                
+                if self.mySet[index] >  num:
+                    
+                    checker = index
+                    
                     break
             
-            self.mySet.insert(test,num)
+            self.mySet.insert(checker,num)
