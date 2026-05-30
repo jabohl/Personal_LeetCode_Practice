40cd69e4cda8d6ec7e75cd1b106a069fbc1ce42c
James Bohl <94003824+jabohl@users.noreply.github.com>
2026-05-30 00:05:22 -0400

    Update AddTwoNumbers.py

--- Arrays/AddTwoNumbers.py
+++ Arrays/AddTwoNumbers.py
@@ -5,22 +5,42 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
         index=0
-        newString=0
+        
+        result=0
+        
         while l1:
-            newString+=((10**index)*l1.val)
-            index+=1
-            l1=l1.next
+            
+            result += ( (10**index) * l1.val )
+            
+            index += 1
+            
+            l1 = l1.next
+        
         index=0
+        
+
+        
         while l2:
-            newString+=((10**index)*l2.val)
-            index+=1
-            l2=l2.next
-        val =newString
-        node=ListNode(int(str(val)[len(str(val))-1]))
-        test=node
-        for i in range(len(str(val))-2,-1,-1):
-            node.next=ListNode(int(str(val)[i]))
+        
+            result += ( (10**index) * l2.val)
+            
+            index += 1
+            
+            l2 = l2.next
+        
+        
+        val = result
+        
+        node = ListNode(int(str(val)[len(str(val))-1]))
+        
+        test = node
+  
+        for index in range(len(str(val))-2,-1,-1):
+
+            node.next = ListNode(int(str(val)[index]))
+            
             node=node.next
         
         return test
