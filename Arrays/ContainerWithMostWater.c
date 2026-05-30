4a527457007a5b73139d27a9ed109888e635d55b
James Bohl <94003824+jabohl@users.noreply.github.com>
2026-05-30 00:09:15 -0400

    Update ContainerWithMostWater.c

--- Arrays/ContainerWithMostWater.c
+++ Arrays/ContainerWithMostWater.c
@@ -1,20 +1,40 @@
 #include <stdio.h>
 
 int maxArea(int* height, int heightSize) {
-    int maxContainer = 0;
-    int i1 = 0;
+    int maxContainer = 0, index = 0, minHeight, maxTest;
+
+    
     int i2 = heightSize - 1;
-    while (i2 != i1) {
-        int minHeight = height[i1] < height[i2] ? height[i1] : height[i2];
-        int maxTest = (i2 - i1) * minHeight;
+    
+    
+    while (i2 != index) {
+    
+        minHeight = height[index] < height[i2] ? height[index] : height[i2];
+        
+        maxTest = (i2 - index) * minHeight;
+        
+        
+        
         if (maxTest > maxContainer) {
             maxContainer = maxTest;
         }
-        if (height[i2] > height[i1]) {
-            i1++;
-        } else {
-            i2--;
+        
+        
+        
+        if (height[i2] > height[index]) {
+            index++ ;
+        } 
+        
+        
+        
+        else {
+            i2-- ;
         }
     }
+
+    
+    
     return maxContainer;
+
+
 }
