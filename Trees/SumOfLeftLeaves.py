50d7352b6894b0d799a5d4e69e3760891b35451e
James Bohl <94003824+jabohl@users.noreply.github.com>
2026-06-01 09:18:47 -0400

    Refactor sumOfLeftLeaves for clarity and efficiency

--- Trees/SumOfLeftLeaves.py
+++ Trees/SumOfLeftLeaves.py
@@ -6,19 +6,49 @@
 #         self.right = right
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
-        lefts, nodeList,testList=[],[],[[root]]
-        i,sum=0,0
-        while len(testList[i]) > 0:
+        
+        lefts, list_of_lefts= [ ], [ [root] ]
+        
+        index, sum = 0, 0
+        
+
+        
+        while len(list_of_lefts[index]) > 0:
+        
             myList=[]
-            for index2 in range(len(testList[i])):   
-                if testList[i][index2].left is not None:
-                    lefts.append(testList[i][index2].left)
-                    myList.append(testList[i][index2].left)
-                if testList[i][index2].right is not None:
-                    myList.append(testList[i][index2].right)
-            testList.append(myList)
-            i+=1
-        for i in range(len(lefts)):
-            if lefts[i].left is None and lefts[i].right is None:
-                sum+=lefts[i].val
+            
+            
+            
+            for index2 in range(len(list_of_lefts[index])):   
+            
+            
+                
+                if list_of_lefts[index][index2].left is not None:
+                    
+                    lefts.append(list_of_lefts[index][index2].left)
+                    myList.append(list_of_lefts[index][index2].left)
+                
+                
+                
+                
+                if list_of_lefts[index][index2].right is not None:
+                    
+                    myList.append(list_of_lefts[index][index2].right)
+            
+            
+            
+            
+            
+            list_of_lefts.append(myList)
+            
+            index += 1
+            
+
+        
+        for index in range(len(lefts)):
+        
+            if lefts[index].left is None and lefts[index].right is None:
+                sum += lefts[index].val
+        
+        
         return sum
