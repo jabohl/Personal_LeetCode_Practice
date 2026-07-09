c2c2aa9d5a9584074feb474e5a8009062f1a9c04
James Bohl <94003824+jabohl@users.noreply.github.com>
2026-07-08 23:12:52 -0400

    Refactor pointers in maxArea function for clarity

--- Arrays/ContainerWithMostWater.js
+++ Arrays/ContainerWithMostWater.js
@@ -2,26 +2,26 @@ var maxArea = function(heightArray) {
     let maxContainerArea = 0;
 
 
-    let leftPointerIndex = 0;
+    let inwardMovingPointer = 0;
 
 
-    let rightPointerIndex = heightArray.length - 1;
+    let outwardMovingPointer = heightArray.length - 1;
 
 
-    while (rightPointerIndex !== leftPointerIndex) {
-        const minHeight = Math.min(heightArray[leftPointerIndex], heightArray[rightPointerIndex]);
+    while (outwardMovingPointer !== inwardMovingPointer) {
+        const minPointerYCoordinates = Math.min(heightArray[inwardMovingPointer], heightArray[outwardMovingPointer]);
 
 
-        const currentContainerArea = (rightPointerIndex - leftPointerIndex) * minHeight;
+        const currentContainerArea = (outwardMovingPointer - inwardMovingPointer) * minPointerYCoordinates;
 
 
         maxContainerArea = Math.max(maxContainerArea, currentContainerArea);
 
 
-        if (heightArray[rightPointerIndex] > heightArray[leftPointerIndex]) {
-            leftPointerIndex++;
+        if (heightArray[outwardMovingPointer] > heightArray[inwardMovingPointer]) {
+            inwardMovingPointer++;
         } else {
-            rightPointerIndex--;
+            outwardMovingPointer--;
         }
     }
 
